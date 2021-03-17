1af5260dbeb9d4a9a50d9ca7319c3d54bf99554f
Sanches <autoscript@gmail.com>
2021-03-17 18:26:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeWorkL15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("L 1.5");
+            bool a = true, b = false, c = false;
+            Console.WriteLine(a | b & !c);
+            Console.WriteLine($"\n{!a & !b}");
+            Console.WriteLine($"\n{!(a & c) | b}");
+            Console.WriteLine($"\n{a & !b | c}");
+            Console.WriteLine($"\n{a | (!(b & c))}");
+
+            Console.WriteLine("\n\nL 1.6");
+            bool x = false, y = false, z = true;
+            Console.WriteLine(x | y & !z);
+            Console.WriteLine($"\n{!x & !y}");
+            Console.WriteLine($"\n{!(x & z) | y}");
+            Console.WriteLine($"\n{x & !y | z}");
+            Console.WriteLine($"\n{x & (!y | z)}");
+            Console.WriteLine($"\n{x | (!(y | z))}");
+
+            Console.WriteLine("\n\nL 1.7");
+            a = true; b = false; c= false; 
+            Console.WriteLine(a | !(a & b) | c);
+            Console.WriteLine($"\n{!a | a & (b | c)}");
+            Console.WriteLine($"\n{(a | b & !c) & c}");
+
+        }
+    }
+}
